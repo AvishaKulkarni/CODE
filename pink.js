7dbd55bf6ee07c7c94e553317ed5fa07e61865c6
Avisha Kulkarni <kulkarniavisha@gmail.com>
2021-10-21 01:58:35 +0530

    Added modified files

---
+++ pink.js
@@ -0,0 +1,7 @@
+console.log("javascript")
+var number = 6
+number = 6.45
+console.log(number)
+
+const arr = [2,"My name",{name: "Avisha"}]
+console.log(arr)
---
+++ yellow.py
@@ -0,0 +1,6 @@
+print("Hello World")
+number = 5
+if number % 2 == 0
+  print("even")
+else:
+    print("")
